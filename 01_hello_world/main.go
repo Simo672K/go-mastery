432b62d02620e71444d523453e2d30519b894146
Simo672K <mohammed.hakmiii555@gmail.com>
2025-02-16 04:11:39 +0100

    01_hello_world

--- 01_hello_world/main.go
+++ 01_hello_world/main.go
@@ -1,5 +1,11 @@
+package main
+
+import "fmt"
+
 // Instructions:
 // Write a program that prints "Hello, World!" to the console.
 // Try to recall the syntax from memory.
 
-
+func main() {
+	fmt.Println("Hello, World!")
+}
